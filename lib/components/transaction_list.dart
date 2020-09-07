65b6e82827fc460a110ce7d7d6035b348e1e329e
Carla Penna <carla.penna@fatec.sp.gov.br>
2020-09-07 17:49:49 -0300

    form validation

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -9,37 +9,44 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tr) {
-        return Card(
-            child: Row(
-          children: [
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.purple, width: 2)),
-              padding: EdgeInsets.all(10),
-              child: Text(
-                'R\$ ${tr.value.toStringAsFixed(2)}',
-                style: TextStyle(fontSize: 20, color: Colors.purple),
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  tr.title,
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (ctx, index) {
+
+          final tr = transactions[index];
+
+          return Card(
+              child: Row(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple, width: 2)),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  'R\$ ${tr.value.toStringAsFixed(2)}',
+                  style: TextStyle(fontSize: 20, color: Colors.purple),
                 ),
-                Text(
-                  DateFormat('d MMM y').format(tr.time),
-                  style: TextStyle(color: Colors.grey),
-                )
-              ],
-            )
-          ],
-        ));
-      }).toList(),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    tr.title,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    DateFormat('d MMM y').format(tr.time),
+                    style: TextStyle(color: Colors.grey),
+                  )
+                ],
+              )
+            ],
+          ));
+        },
+      ),
     );
   }
 }
--- lib/pages/screen.dart
+++ lib/pages/screen.dart
@@ -2,24 +2,25 @@ import 'package:expenses/components/transaction_user.dart';
 import 'package:flutter/material.dart';
 
 class Screen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Despesas Pessoais"),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            child: Card(
-              elevation: 5,
-              child: Text("Texto 1"),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: Card(
+                elevation: 5,
+                child: Text("Texto 1"),
+              ),
             ),
-          ),
-          TransactionUser(),
-        ],
+            TransactionUser(),
+          ],
+        ),
       ),
     );
   }
